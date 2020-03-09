5c7c45974fc307a3e39d9a9a4840a151a99ce916
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-03-09 23:03:04 +0800

    for loop aws managed polices

--- modules/iam_policies/aws_managed_policies.tf
+++ modules/iam_policies/aws_managed_policies.tf
@@ -21,35 +21,42 @@ locals {
       )
     )
   }
-  aws_managed_policy_arns = {
-    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
-    Billing = "arn:aws:iam::aws:policy/job-function/Billing"
+  aws_managed_policies_details = {
+    AdministratorAccess = {
+      policy_arn         = "arn:aws:iam::aws:policy/AdministratorAccess",
+      policy_name        = "AdministratorAccess",
+      policy_description = "Same as AWS Managed AdministratorAccess policy, but can config with MFA."
+    }
+    Billing = {
+      policy_arn         = "arn:aws:iam::aws:policy/job-function/Billing",
+      policy_name        = "Billing",
+      policy_description = "Same as AWS Managed Billing policy, but can config with MFA."
+    }
   }
   //out-of-box IAM policies, which is used for attaching to groups
   output_policy_map = {
-    AdministratorAccess = {
-      policy_name        = "AdministratorAccess",
-      policy_description = "Same as AWS Managed AdministratorAccess policy, but can config with MFA.",
-      policy_doc         = data.aws_iam_policy_document.full_access.json
-    }
-    Billing = {
-      policy_name        = "Billing",
-      policy_description = "Same as AWS Managed Billing policy, but can config with MFA.",
-      policy_doc         = data.aws_iam_policy_document.billing_access.json
-    }
+    for k, v in local.aws_managed_policies_details :
+    k => merge(
+      v, {
+        policy_doc = data.aws_iam_policy_document.aws_managed_policies_with_mfa_option[k].json
+    })
   }
 }
 
 data "aws_iam_policy" "aws_managed_policies" {
-  for_each = local.aws_managed_policy_arns
-  arn      = each.value
+  for_each = local.aws_managed_policies_details
+  arn      = each.value.policy_arn
+}
+data "aws_iam_policy_document" "aws_managed_policies_with_mfa_option" {
+  for_each      = local.aws_managed_policies
+  source_json   = local.first_time_login_without_mfa_json
+  override_json = each.value
 }
 
 /*
 Create full_access policy.
 
 This policy will give admin permissions to user. Attach it to group and add user to that group 
-*/
 
 data "aws_iam_policy_document" "full_access" {
   source_json   = local.first_time_login_without_mfa_json
@@ -60,6 +67,7 @@ data "aws_iam_policy_document" "billing_access" {
   source_json   = local.first_time_login_without_mfa_json
   override_json = local.aws_managed_policies["Billing"]
 }
+*/
 
 // aws managed billing policy
 
