9b4bf3c00e96a2d76e55820ef979d3d98be1c180
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-03-14 12:15:24 +0800

    format terraform code

--- examples/iam_across_account_assistant/main.tf
+++ examples/iam_across_account_assistant/main.tf
@@ -9,10 +9,10 @@ provider "aws" {
 
 //create role
 module "iam_across_account_assistant" {
-  source        = "../../modules/iam_across_account_assistant"
+  source                                         = "../../modules/iam_across_account_assistant"
   allow_read_only_access_from_other_account_arns = []
-  should_require_mfa = true
-  across_account_access_role_arns_by_group = {}
+  should_require_mfa                             = true
+  across_account_access_role_arns_by_group       = {}
   user_groups = [{
     group_name = "billing"
     user_profiles = [
