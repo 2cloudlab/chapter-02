aa9c984ec6f64977c830fcab216714595326f6a7
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-03-15 17:46:38 +0800

    update test case

--- test/iam_across_account_assistant_test.go
+++ test/iam_across_account_assistant_test.go
@@ -1,17 +1,43 @@
 package test
 
-import(
-	"github.com/gruntwork-io/terratest/modules/terraform"
+import (
+	"path/filepath"
 	"testing"
+	"fmt"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/gruntwork-io/terratest/modules/random"
 )
 
-func TestAlbExample(t *testing.T) {
-	fmt.Println()
-	fmt.Println("If you see this text, it's working!")
-	fmt.Println()
-}
-
 func TestIntegrationIAM2Groups(t *testing.T) {
+	//Make this test case parallel which means it will not block other test cases
 	t.Parallel()
-	testCouchbaseMultiCluster(t, "ubuntu", "enterprise")
+	//Copy folder "../" to a tmp folder and return the tmp path of "examples"
+	examplesFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples")
+	iam_across_account_assistantFolder := filepath.Join(examplesFolder, "iam_across_account_assistant")
+
+	//Create terraform options which is passed to terraform module
+	user_groups := []map[string]interface{}{
+		{
+			"group_name": "full_access",
+			"user_profiles": []map[string]interface{}{
+				"pgp_key":   fmt.Sprintf("pgp-%s", random.UniqueId()),
+				"user_name": fmt.Sprintf("username-%s", random.UniqueId()),
+			},
+		},
+	}
+	terraformOptions := &terraform.Options{
+		TerraformDir: iam_across_account_assistantFolder,
+		Vars: map[string]interface{}{
+			"should_require_mfa": true,
+			"user_groups":        user_groups,
+		},
+	}
+
+	//Something like finally in try...catch
+	defer terraform.Destroy(t, terraformOptions)
+
+	//Something like terraform init and terraform apply
+	terraform.InitAndApply(t, terraformOptions)
 }
