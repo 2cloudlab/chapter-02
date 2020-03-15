0178c1ea4f4e04e35fa260e8dcd7e2964284c24a
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-03-15 22:24:12 +0800

    update with assert

--- test/iam_across_account_assistant_test.go
+++ test/iam_across_account_assistant_test.go
@@ -8,8 +8,10 @@ import (
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/gruntwork-io/terratest/modules/aws"
 )
 
+//Create full_access group with admin permissions and config with MFA option
 func TestIntegrationIAM2Groups(t *testing.T) {
 	//Make this test case parallel which means it will not block other test cases
 	t.Parallel()
@@ -18,14 +20,16 @@ func TestIntegrationIAM2Groups(t *testing.T) {
 	iam_across_account_assistantFolder := filepath.Join(examplesFolder, "iam_across_account_assistant")
 
 	//Create terraform options which is passed to terraform module
+	expected_group_name := "full_access"
+	expected_user_name := fmt.Sprintf("username-%s", random.UniqueId())
 	user_groups := []map[string]interface{}{
 		{
-			"group_name": "full_access",
+			"group_name": expected_group_name,
 			"user_profiles": []map[string]interface{}{
 				{
 					//Use random.UniqueId() to make input value uniqued!
 					"pgp_key":   fmt.Sprintf("pgp-%s", random.UniqueId()),
-					"user_name": fmt.Sprintf("username-%s", random.UniqueId()),
+					"user_name": expected_user_name,
 				},
 			},
 		},
@@ -43,4 +47,18 @@ func TestIntegrationIAM2Groups(t *testing.T) {
 
 	//Something like terraform init and terraform apply
 	terraform.InitAndApply(t, terraformOptions)
+
+	//Validate the created group
+	client := aws.NewIamClient(t, "region")
+
+	resp, err := iamClient.GetUser(&iam.GetGroupInput{
+		"GroupName" : expected_group_name
+	})
+	if err != nil {
+		return "", err
+	}
+	actual_group_name := *resp.Group.GroupName
+	assert.Equal(t, expected_group_name, actual_group_name, "These 2 groups should be the same.")
+	actual_user_name := *resp.Users[0].UserName
+	assert.Equal(t, expected_user_name, actual_user_name, "These 2 user names should be the same.")
 }
